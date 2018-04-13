fd35871fc3047da673d2baab8da3266e7c79af14
Raphael Guedes <rafael_sg95@hotmail.com>
2018-04-13 13:52:50 -0300

    Add files

---
+++ src/main/java/br/com/mycompany/controller/PessoaMB.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mycompany.controller;
+
+import br.com.mycompany.model.Pessoa;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author Raphael
+ */
+@ManagedBean
+@ViewScoped
+public class PessoaMB implements Serializable{
+
+    private Pessoa pessoa = new Pessoa();
+    private List<Pessoa> pessoaLista = new ArrayList<Pessoa>();
+    
+    public PessoaMB() {
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public List<Pessoa> getPessoaLista() {
+        return pessoaLista;
+    }
+
+    public void setPessoaLista(List<Pessoa> pessoaLista) {
+        this.pessoaLista = pessoaLista;
+    }
+    
+    public void salvarPessoa(){
+        pessoaLista.add(pessoa);
+        pessoa = new Pessoa();
+    }
+}
---
+++ src/main/java/br/com/mycompany/model/Pessoa.java
@@ -0,0 +1,49 @@
+package br.com.mycompany.model;
+
+public class Pessoa {
+    private String nome;
+    private String endereco;
+    private String numero;
+    private String telefone;
+    private String cidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
